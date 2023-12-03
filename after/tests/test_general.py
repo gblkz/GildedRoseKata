6458f8b3f69a3fa5bae2cabff177339a98883b66
Gordon Lippa <gordon.lippa@gmail.com>
2023-12-02 21:01:56 -0800

    Comment: verify all tests wrk before adding new.

--- after/tests/test_general.py
+++ after/tests/test_general.py
@@ -7,7 +7,8 @@ from after.gilded_rose import (
 AGED_BRIE = "Aged Brie"
 
 
-# Watch Arjan's PyTest unit testing video. Verify before adding new tests.
+# Watch Arjan's PyTest unit testing video.
+# Verify all tests work before adding new tests.
 def test_item_doesnt_change_name():
     item = Item("foo", 0, 0)
     update_quality([item])
