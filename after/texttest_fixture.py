c388e2b983989249eed19ed617202d20cc7068a1
Gordon Lippa <gordon.lippa@gmail.com>
2023-12-21 19:33:16 -0800

    Debug texttest_future for bad reference.

--- after/texttest_fixture.py
+++ after/texttest_fixture.py
@@ -26,5 +26,5 @@ if __name__ == "__main__":
         for item in items:
             print(item)
         print("")
-        # Is this deliberate typo, did I mess it up?
+        # Is this deliberate typo?
         GildedRose(items).update_quality()
