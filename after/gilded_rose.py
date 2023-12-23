dfd96207b4a9ca61c688b73a000027ce1ef66675
Gordon Lippa <gordon.lippa@gmail.com>
2023-12-22 19:52:36 -0800

    Variables refactor.

--- after/gilded_rose.py
+++ after/gilded_rose.py
@@ -10,10 +10,11 @@ def decrease_item_quality(item: Item, amount: int = 1) -> None:
 def increase_item_quality(item: Item, amount: int = 1, max_quality: int = 50) -> None:
     item.quality = min(item.quality + amount, max_quality)
 
-# Abstract variables, no hardcoded values.
+# Abstract variables.
 AGED_BRIE = "Aged Brie"
 BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert"
 SULFURAS = "Sulfuras, Hand of Ragnaros"
+# CONJURED = "Conjured Mana Cake"
 CONJURED = "Conjured Mana Cake"
 
 
