cb4cac678c83d3b7f57e07846fc08ff8acab5bac
Gordon Lippa <gordon.lippa@gmail.com>
2024-01-03 20:16:29 -0800

    Marking duplicate commented out code for review.

--- after/tests/test_general.py
+++ after/tests/test_general.py
@@ -7,6 +7,7 @@ AGED_BRIE = "Aged Brie"
 
 
 # Watch Arjan's video PyTest unit testing, then extend these.
+# Watch Arjan's video PyTest unit testing, then extend these.
 def test_item_doesnt_change_name():
     item = Item("foo", 0, 0)
     update_quality([item])
