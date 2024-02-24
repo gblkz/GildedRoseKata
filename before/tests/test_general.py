fcbad51748056093d796a273805221fd0275086e
Gordon Lippa <gordon.lippa@gmail.com>
2024-02-23 22:11:02 -0800

    Update to comments explaining the before issues corrected in after.

--- before/tests/test_general.py
+++ before/tests/test_general.py
@@ -4,6 +4,8 @@ from before.gilded_rose import (
 )
 
 
+# Still discovering the best approach: edit the before OR comment but leave as-is.
+# Current feeling is leave and comment. The after-folder has the changes, before comments why.
 def test_fixme():
     item = Item("foo", 0, 0)
     update_quality([item])
