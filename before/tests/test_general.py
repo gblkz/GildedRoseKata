515cef8cb87278390724df50ddc66564cf3e60ef
Gordon Lippa <gordon.lippa@gmail.com>
2024-02-27 21:00:52 -0800

    Comment to use design docs over comments.

--- before/tests/test_general.py
+++ before/tests/test_general.py
@@ -6,6 +6,7 @@ from before.gilded_rose import (
 
 # Still discovering the best approach: edit the before OR comment but leave as-is.
 # Current feeling is leave and comment. The after-folder has the changes, before comments why.
+# Need to stop using comments for note capture.
 def test_fixme():
     item = Item("foo", 0, 0)
     update_quality([item])
