186d81ff6ab692be291ea8fc48846112b6380891
Gordon Lippa <gordon.lippa@gmail.com>
2024-03-09 21:18:00 -0800

    Refactor our Git process.

--- after/tests/test_general.py
+++ after/tests/test_general.py
@@ -7,7 +7,8 @@ AGED_BRIE = "Aged Brie"
 
 
 # Watch Arjan's video PyTest unit testing, then extend these and integrate with automation.
-# Watch for how Arjan executes his tests, then replicate the engine for this project.
+# Watch for how Arjan executes his tests, then replicate for this project.
+# This is getting old.
 def test_item_doesnt_change_name():
     item = Item("foo", 0, 0)
     update_quality([item])
